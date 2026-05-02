e7002753d39d68986fbfaa6036e03fe31e36d050
Tolik1222 <a.y.smirnov@student.khai.edu>
2026-05-02 22:38:51 +0300

    build.sh edited

--- build.sh
+++ build.sh
@@ -5,7 +5,8 @@ set -o errexit
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
+
 python manage.py migrate
 
 export DJANGO_SUPERUSER_PASSWORD='doter222'
-python manage.py createsuperuser --noinput --username admin --email admin@example.com || true
+python manage.py createsuperuser --noinput --username doter222 --email admin@example.com || true
